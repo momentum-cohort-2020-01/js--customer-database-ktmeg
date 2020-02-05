07cd2189affeb7bdea05248259d2e7159f895927
Kat <kmneil2@gmail.com>
2020-02-04 21:38:44 -0500

    re-start

--- script.js
+++ script.js
@@ -1,26 +1,56 @@
-function capitalizeFirstLetter (string) {
-  return string[0].toUpperCase() + string.slice(1)
+// function capitalizeFirstLetter (string) {
+//   return string[0].toUpperCase() + string.slice(1)
+// }
+
+// const customerNames = customers.map(function (customer) {
+//   return capitalizeFirstLetter(customer.name.first) +
+//     ' ' +
+//     capitalizeFirstLetter(customer.name.last)
+// })
+
+// const customerListItems = customerNames.map(function (name) {
+//   const li = document.createElement('li')
+//   li.textContent = name
+//   return li
+// })
+
+// console.log(customerNames)
+
+// const ul = document.createElement('ul')
+// for (const li of customerListItems) {
+//   ul.appendChild(li)
+// }
+
+// document.querySelector('#customers').appendChild(ul)
+
+// // const customerPictures = customers.map(function (customer) {
+// //   const img = document.createElement('img')
+// //     img.src = customer.picture.large
+// //     return img
+// // })
+const database = document.querySelector('#database')
+
+function customerProfiles() {
+  for (let customer of customers) {
+    const profile = document.createElement('div')
+    profile.classList.add('profile')
+
+    const pic = document.createElement('img')
+    pic.classList.add('pic')
+    pic.src = customer.picture.large
+    profile.appendChild(pic)
+
+    const name = document.createElement('h2')
+    name.classList.add('name')
+    name.textContent = name.first + ' ' + name.last
+    profile.appendChild(name)
+
+    const address = document.createElement('p')
+    location.classList.add('address')
+    location.textContent = location.street + location.city + nameToAbbr(location.state) + location.postcode
+    profile.appendChild(address)
 }
 
-const customerNames = customers.map(function (customer) {
-  return capitalizeFirstLetter(customer.name.first) +
-    ' ' +
-    capitalizeFirstLetter(customer.name.last)
-})
-
-const customerListItems = customerNames.map(function (name) {
-  const li = document.createElement('li')
-  li.textContent = name
-  return li
-})
-
-console.log(customerNames)
-
-const ul = document.createElement('ul')
-for (const li of customerListItems) {
-  ul.appendChild(li)
+database.appendChild('profile')
 }
-
-document.querySelector('#customers').appendChild(ul)
-
-
+customerProfiles()
